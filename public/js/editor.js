6a4241c51f558da5dc212fde1c8f44010e49a2ee
stefnotadevyet <srjack91@gmail.com>
2022-02-14 13:21:58 -0500

    need to figure out why upload isnt showing

--- public/js/editor.js
+++ public/js/editor.js
@@ -7,5 +7,27 @@ const banner = document.querySelector('.banner');
 let bannerPath;
 
 const publishBtn = document.querySelector('.publish-btn');
-const U
+const uploadInput = document.querySelector('#image-upload');
+
+bannerImage.addEventListener('change', () => {
+  uploadImage(bannerImage, 'banner');
+})
+
+const uploadImage = (uploadFile, uploadType) => {
+  const [file] = uploadFile.files;
+  if (file && file.type.includes('image')) {
+    const formData = new FormData();
+    formData.append('image', file)
+
+    fetch('/upload', {
+      method: 'post',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+      bannerPath = `${location.origin}/${data}`;
+      banner.getElementsByClassName.backgroungImage = `url("${bannerPath}")`
+    })
+  }
+}
 
