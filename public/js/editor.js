666c8f8614b6077839fba5e95609452b433fce8c
stefnotadevyet <srjack91@gmail.com>
2022-02-14 13:01:02 -0500

    added route for image upload

---
+++ public/js/editor.js
@@ -0,0 +1,11 @@
+const blogTitleField = document.querySelector('.title');
+const articleField = document.querySelector('.article');
+
+// banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector('.banner');
+let bannerPath;
+
+const publishBtn = document.querySelector('.publish-btn');
+const U
+
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 
 let initial_path = path.join(__dirname, 'public');
 
 const app = express();
 app.use(express.static(initial_path));
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(initial_path, 'home.html'));
@@ -12,6 +14,27 @@ app.get('/', (req, res) => {
 
 app.get('/editor', (req, res) => {
   res.sendFile(path.join(initial_path, 'editor.html'))
+})
+
+// upload link
+
+app.post('/upload', (req, res) => {
+  let file = req.files.image;
+  let date = new Date();
+  //adds the date, time and filename to get a unique image name
+  let imageName = date.getDate() + date.getTime() + file.name;
+  // image upload path
+  let path = `public/uploads/${imageName}`;
+
+  // create upload
+  file.mv(path, (err, result) => {
+    if (err) {
+      throw err
+    } else {
+      // the image upload path
+      res.json(`uploads/${imageName}`)
+    }
+  })
 })
 
 app.listen('3000', () => {
